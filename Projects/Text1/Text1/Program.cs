cc1f72af5b91befb352021ef2af3a8916d98f7ca
Ken Takei <95737638+tkikn0211@users.noreply.github.com>
2022-09-30 17:19:01 +0900

    first commit

---
+++ Projects/Text1/Text1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Text1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("!!!!!!!!!");
+
+            Console.Write("What is your name?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello," + name + "!");
+        }
+    }
+}
