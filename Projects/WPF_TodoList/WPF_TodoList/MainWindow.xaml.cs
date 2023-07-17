c8cc77f06392792c4a5c3b42c218db0925020a56
Ken Takei <95737638+tkikn0211@users.noreply.github.com>
2023-07-17 11:13:04 +0900

    新規追加

---
+++ Projects/WPF_TodoList/WPF_TodoList/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿// 前提条件：WPF アプリケーションのプロジェクトを作成する
+
+using System.ComponentModel;
+
+namespace TodoList
+{
+    public partial class MainWindow : Window
+    {
+        private BindingList<TodoElement> todoList = new BindingList<TodoElement>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void SetBindingList(object sender, RoutedEventArgs e)
+        {
+            TodoList.ItemsSource = todoList;
+        }
+        public void AddTodoList(object sender, RoutedEventArgs e)
+        {
+            todoList.Add(new TodoElement(TodoText.Text, false));
+        }
+        public void DeleteTodoList(object sender, RoutedEventArgs e)
+        {
+            BindingList<TodoElement> ListTmp = new BindingList<TodoElement>();
+            foreach (TodoElement element in todoList)
+            {
+                ListTmp.Add(element);
+            }
+            foreach (TodoElement element in ListTmp)
+            {
+                if (element.DoneFlg) todoList.Remove(element);
+            }
+        }
+    }
+}
---
+++ Projects/WPF_TodoList/WPF_TodoList/TodoElement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoList
+{
+    class TodoElement
+    {
+        public TodoElement(string Element, bool DoneFlg)
+        {
+            this.Element = Element;
+            this.DoneFlg = DoneFlg;
+        }
+        public string Element { get; set; }
+        public bool DoneFlg { get; set; }
+    }
+}
